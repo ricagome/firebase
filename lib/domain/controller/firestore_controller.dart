83b2c5016e156ee8873d62928017b622c3e161c9
Ricardo Gomez <ricardo_a_gomez@hotmail.com>
2021-12-03 20:17:48 -0500

    Upadate

--- lib/domain/controller/firestore_controller.dart
+++ lib/domain/controller/firestore_controller.dart
@@ -7,9 +7,13 @@ class FirebaseController extends GetxController {
   var _records = <Record>[].obs;
 
   List<Record> get entries => _records;
-  
-  //Implementa los getters necesarios para los datos y el stream
 
+  //Implementa los getters necesarios para los datos y el stream
+  final CollectionReference baby =
+      FirebaseFirestore.instance.collection('baby');
+
+  final Stream<QuerySnapshot> _userStream =
+      FirebaseFirestore.instance.collection('baby').snapshots();
 
   @override
   void onInit() {
@@ -18,8 +22,23 @@ class FirebaseController extends GetxController {
   }
 
   //Implementa el metodo para suscribirse a cambios en lo datos
-  suscribeUpdates() async {}
+  suscribeUpdates() async {
+    logInfo('suscribeLocationUpdates');
+    _userStream.listen((event) {
+      logInfo('Ir a nuevo Item de firesotre');
+      _records.clear();
+      event.docs.forEach((element) {
+        _records.add(Record.fromSnapshot(element));
+      });
+      print('${_records.length}');
+    });
+  }
 
   //Implementa el metodo para agregar datos en firestore
-  addEntry(name) { }
+  addEntry(name) {
+    baby
+        .add({'name': name, 'votes': 0})
+        .then((value) => print('Bebe agregado'))
+        .catchError((onError) => print('Error al agregar el bebe'));
+  }
 }
